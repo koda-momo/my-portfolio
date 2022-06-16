893a39cbddc58af232b01d5a09fa73f40a13c265
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-16 18:25:03 +0900

    appのheadタイトルにも適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Layout } from "../components/Layout";
+import { useMakeTitle } from "../hooks/useMakeTitle";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { makeTitle } = useMakeTitle();
+
   return (
     <>
       <Head>
-        <title>ここにタイトルを入れる</title>
+        <title>portfolio:{makeTitle()}</title>
         <meta name="description" content="サイトの説明を入れる" />
         {/* <link rel="icon" href="/images/favicon.jpg" /> */}
       </Head>
