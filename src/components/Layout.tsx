e271b0e42ae1fa780a0ec9edc218c22f5e988b4f
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-17 14:33:00 +0900

    全体のバランス調整

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "./Header";
 //MUI
 import { styled } from "@mui/material/styles";
 import { useMakeTitle } from "../hooks/useMakeTitle";
+import { Menubar } from "./Menubar";
 
 type Props = {
   children: ReactNode;
@@ -21,13 +22,29 @@ export const Layout: FC<Props> = memo(({ children }) => {
   return (
     <>
       <Header title={makeTitle()} />
-      <_Main>{children}</_Main>
+      <main></main>
+      <_Main>
+        <_Contents>{children}</_Contents>
+
+        <_Menu>
+          <Menubar />
+        </_Menu>
+      </_Main>
     </>
   );
 });
 
-const _Main = styled("main")(() => ({
+const _Main = styled("div")(() => ({
   position: "absolute",
   left: 50,
   top: 150,
+  display: "flex",
+  gap: "10%",
+  width: "90%",
 }));
+
+const _Contents = styled("div")(() => ({
+  width: "100%",
+}));
+
+const _Menu = styled("div")(() => ({}));
