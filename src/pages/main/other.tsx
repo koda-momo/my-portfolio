2f3afd6aad0938e4d5a6d21283157f8963b518bc
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-20 09:50:01 +0900

    SSGでQiitaデータ取得メソッド追加

--- src/pages/main/other.tsx
+++ src/pages/main/other.tsx
@@ -1,10 +1,28 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 /**
  * その他情報ページ.
  */
-const Other: NextPage = () => {
+const Other: NextPage<Props> = ({ data }) => {
+  console.dir(data);
   return <>Other</>;
+};
+
+/**
+ * Qiitaデータの取得.
+ */
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://qiita.com/api/v2/users/koda-momo");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 10,
+  };
 };
 
 export default Other;
