20b9763f789413fb4a1c6439a8093d390a95da51
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-06-20 16:21:04 +0900

    qiita記事コンポーネントCSS調整

--- src/components/QiitaList.tsx
+++ src/components/QiitaList.tsx
@@ -1,7 +1,14 @@
 import { FC, memo } from "react";
-import { QiitaType } from "../types/qiitaType";
 import Link from "next/link";
+
+//components
 import { useFormatDate } from "../hooks/useFormatDate";
+import { QiitaType } from "../types/qiitaType";
+
+//MUI
+import { styled } from "@mui/material/styles";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
 type Props = {
   data: QiitaType;
@@ -15,13 +22,62 @@ export const QiitaList: FC<Props> = memo(({ data }) => {
 
   return (
     <>
-      <Link href={data.url}>
-        <a>{data.title}</a>
-      </Link>
-      <div>いいね数:{data.likes_count}</div>
-      <div>コメント数:{data.comments_count}</div>
-      <div>作成日:{formatDate(data.created_at)}</div>
-      <div>更新日:{formatDate(data.updated_at)}</div>
+      <_Qiita>
+        <Link href={data.url}>
+          <_A>
+            {data.title}
+
+            <_Flex>
+              {/* いいね */}
+              <_Icon>
+                <_IconColor>
+                  <ThumbUpIcon />
+                </_IconColor>
+                {data.likes_count}
+              </_Icon>
+
+              {/* コメント */}
+              <_Icon>
+                <_IconColor>
+                  <ChatBubbleIcon />
+                </_IconColor>
+
+                {data.comments_count}
+              </_Icon>
+
+              <div>作成日:{formatDate(data.created_at)}</div>
+              <div>更新日:{formatDate(data.updated_at)}</div>
+            </_Flex>
+          </_A>
+        </Link>
+      </_Qiita>
     </>
   );
 });
+
+const _Qiita = styled("div")(() => ({
+  backgroundColor: "white",
+  border: "dashed 2px #C7D36F",
+  marginBottom: 30,
+  padding: 20,
+}));
+
+const _Icon = styled("div")(() => ({
+  display: "flex",
+  gap: 3,
+}));
+
+const _IconColor = styled("div")(() => ({
+  color: "#9EB23B",
+}));
+
+const _Flex = styled("div")(() => ({
+  display: "flex",
+  gap: 20,
+}));
+
+const _A = styled("a")(() => ({
+  ":hover": {
+    opacity: "40%",
+  },
+}));
