8bd115933dbc6686b3624420a8fedb3fb2fbd59b
iimomo <epotetoco3@gmail.com>
2025-02-16 14:54:12 +0900

    ハンバーガーメニューの作成

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,12 +1,15 @@
-import { FC } from "react";
+import clsx from "clsx";
+import { FC, useState } from "react";
 import { pageNavList } from "~/const/top";
 
 /**
  * Headerコンポーネント.
  */
 export const Header: FC = () => {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
   return (
-    <header className="px-10 h-28 flex gap-20 items-center">
+    <header className="relative px-10 h-28 flex lg:gap-20 items-center justify-between lg:justify-start">
       <a href="/" className="hover:opacity-70">
         <img
           src="/top/top-title.png"
@@ -15,7 +18,32 @@ export const Header: FC = () => {
         />
       </a>
 
-      {/* TODO:ハンバーガーメニュー化 */}
+      <div className="lg:hidden">
+        <button
+          className={clsx(
+            "text-4xl z-[20] absolute top-5",
+            hamburgerOpen && "text-secondary"
+          )}
+          onClick={() => setHamburgerOpen((prev) => !prev)}
+        >
+          {hamburgerOpen ? "×" : "≡"}
+        </button>
+
+        {hamburgerOpen && (
+          <nav className="bg-tertiary text-primary p-5 pt-16 z-[10] absolute top-0 right-0 w-[50%] h-[100vh]">
+            <ul>
+              {pageNavList.map(({ url, name }) => (
+                <li key={name}>
+                  <a href={url} className="py-2 w-[100%] h-[100%] block">
+                    {name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </div>
+
       <nav className="hidden lg:block">
         <ul className="flex lg:gap-5">
           {pageNavList.map(({ url, name }) => (
